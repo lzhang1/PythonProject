20928a07ed309c309d1edffb4bd558c485ec3d10
leizhang <lzhang@redhat.com>
2017-03-20 18:14:26 +0800

    update alien project

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,20 +2,23 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = ai_settings.bg_color
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(bg_color)
-        pygame.display.flip()
-
+        #screen.blit(bg_image, (0,0))
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
